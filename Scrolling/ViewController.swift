f8cb1e1b5d612ae454637d2d092d1462a3be9036
Alexander Zubkov <3323310@gmail.com>
2017-10-16 19:12:32 +0300

    Initial Commit

---
+++ Scrolling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scrolling
+//
+//  Created by Alexander Zubkov on 16.10.17.
+//  Copyright © 2017 Alexander Zubkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
